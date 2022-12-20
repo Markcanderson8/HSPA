d795f44970046021cc93e9c6516fae6bf2b240c7
Mark Anderson <manderson@diesellaptops.com>
2022-12-20 10:46:15 -0500

    finished all cards and need to work on formatting

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit{
 
+  properties: Array<any> = [
+  {
+    "Id":1,
+    "Name": "Mark's House",
+    "Type": "House",
+    "Price": 12000
+  },
+  {
+    "Id":2,
+    "Name": "Michael's House",
+    "Type": "House",
+    "Price": 5000
+  },
+  {
+    "Id":3,
+    "Name": "Maddy's House",
+    "Type": "House",
+    "Price": 100000
+  },
+  {
+    "Id":4,
+    "Name": "Mom's House",
+    "Type": "House",
+    "Price": 35000
+  },
+  {
+    "Id":5,
+    "Name": "Cheryl's House",
+    "Type": "House",
+    "Price": 112000
+  },
+  {
+    "Id":6,
+    "Name": "Jenny's House",
+    "Type": "House",
+    "Price": 172000
+  },
+]
+
+constructor() {}
+
+ngOnInit(): void {
+
+}
 }
