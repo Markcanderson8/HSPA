f4a1934b26c7d3b8f88fa815c2cc46e632765183
Mark Anderson <manderson@diesellaptops.com>
2022-12-21 08:15:45 -0500

    Got the cards hashed out
    Add property card list

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -8,44 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class PropertyListComponent implements OnInit{
 
   properties: Array<any> = [
-  {
-    "Id":1,
-    "Name": "Mark's House",
-    "Type": "House",
-    "Price": 12000
-  },
-  {
-    "Id":2,
-    "Name": "Michael's House",
-    "Type": "House",
-    "Price": 5000
-  },
-  {
-    "Id":3,
-    "Name": "Maddy's House",
-    "Type": "House",
-    "Price": 100000
-  },
-  {
-    "Id":4,
-    "Name": "Mom's House",
-    "Type": "House",
-    "Price": 35000
-  },
-  {
-    "Id":5,
-    "Name": "Cheryl's House",
-    "Type": "House",
-    "Price": 112000
-  },
-  {
-    "Id":6,
-    "Name": "Jenny's House",
-    "Type": "House",
-    "Price": 172000
-  },
-]
-
+    {
+      "Id": 1,
+      "Name": "Mark's",
+      "Type": "House",
+      "Price": 412000
+    },
+    {
+      "Id": 2,
+      "Name": "Maddy's",
+      "Type": "House",
+      "Price": 312000
+    },
+    {
+      "Id": 3,
+      "Name": "Mom's",
+      "Type": "House",
+      "Price": 112000
+    },
+    {
+      "Id": 4,
+      "Name": "Dad's",
+      "Type": "House",
+      "Price": 120000
+    },
+    {
+      "Id": 5,
+      "Name": "Chance's",
+      "Type": "House",
+      "Price": 100000
+    },
+    {
+      "Id": 6,
+      "Name": "Michael's",
+      "Type": "House",
+      "Price": 150000
+    },
+  ]
 constructor() {}
 
 ngOnInit(): void {
